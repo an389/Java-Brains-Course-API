0ab9d7835fea8e95e953571692d8a2e9d4f90288
Mihai Andrei-Iulian <62914848+an389@users.noreply.github.com>
2023-02-04 16:18:49 +0200

    start

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
+
     }
 }
